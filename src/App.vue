42add3ea829ef31a3d48e3fa413f36b9b645bab0
Tatul <tatulkhachatryanait@gmail.com>
2022-03-04 17:05:49 +0400

    registracia button@ patrast

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <router-view />
+  <login-page />
+  <!-- <router-view /> -->
 </template>
 <script>
+import LoginPage from "./component/login-Page.vue";
 export default {
+  components: {
+    LoginPage,
+  },
   name: "App",
 };
 </script>
---
+++ src/component/login-Page.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="login-main">
+    <div class="q-pa-md">
+      <div class="row">
+        <div class="image col fit row wrap justify-center items-center">
+          <div class="twitter-icon"></div>
+        </div>
+        <div class="container col fit row wrap justify-center items-center">
+          <div class="content">
+            <div class="mini-icon-div justify-start">
+              <div class="mini-icon"></div>
+            </div>
+            <div class="big-title text-h1 text-weight-bolder">
+              Happening now
+            </div>
+            <div class="mini-title text-h3 text-weight-bolder">
+              Join Twitter today.
+            </div>
+            <div class="buttons fit column wrap justify-start items-start">
+              <q-btn outline rounded border-color="#dadce0" class="btn1" md>
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 48 48"
+                  class="gmail-icon"
+                >
+                  <g>
+                    <path
+                      fill="#EA4335"
+                      d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"
+                    ></path>
+                    <path
+                      fill="#4285F4"
+                      d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"
+                    ></path>
+                    <path
+                      fill="#FBBC05"
+                      d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"
+                    ></path>
+                    <path
+                      fill="#34A853"
+                      d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"
+                    ></path>
+                    <path fill="none" d="M0 0h48v48H0z"></path>
+                  </g>
+                </svg>
+                Sign up with Google</q-btn
+              >
+              <q-btn
+                outline
+                rounded
+                color="#dadce0"
+                class="btn2 text-weight-bolder"
+                ><div class="apple"></div>
+                Sign up with Apple</q-btn
+              >
+            </div>
+            <div class="line row justify-center items-center">or</div>
+            <q-btn
+              unelevated
+              rounded
+              color="primary"
+              class="btn3 text-weight-bolder"
+              >Sign up with phone or email</q-btn
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.image {
+  /* background-color: aquamarine; */
+  background-image: url("../../public/login-image.png");
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+/* .container {
+  /* background-color: blueviolet; */
+/* } */
+.row {
+  height: 100vh;
+}
+.twitter-icon {
+  /* -webkit-mask-image: url("../../public/twitter-white.png"); */
+  mask-image: url("../../public/twitter-white.png");
+  -webkit-mask-size: 100%;
+  mask-size: 100%;
+  width: 500px;
+  height: 600px;
+  -webkit-mask-repeat: no-repeat;
+  mask-repeat: no-repeat;
+  background-color: #fff;
+  /* color: #fff; */
+}
+.mini-icon {
+  mask-image: url("../../public/twitter-white.png");
+  -webkit-mask-size: 100%;
+  mask-size: 100%;
+  width: 100px;
+  height: 100px;
+  -webkit-mask-repeat: no-repeat;
+  mask-repeat: no-repeat;
+  background-color: rgb(29, 155, 240);
+}
+.mini-icon-div {
+  width: 90%;
+}
+.big-title {
+  font-family: Arial, Helvetica, sans-serif;
+  margin-bottom: 48px;
+  margin-top: 48px;
+}
+.mini-title {
+  margin-bottom: 32px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.btn1 {
+  margin-bottom: 10px;
+  width: 400px;
+  height: 50px;
+}
+.apple {
+  background-image: url("../../public/apple.png");
+  width: 30px;
+  height: 30px;
+  background-size: 100% 100%;
+  margin-right: 10px;
+}
+.btn2 {
+  width: 400px;
+  height: 50px;
+}
+.btn1:hover {
+  border: noone;
+  border: 1px solid rgba(39, 128, 139);
+  background-color: rgb(230, 255, 252, 1) !important;
+}
+.gmail-icon {
+  width: 30px;
+  height: 30px;
+  margin-right: 15px;
+}
+.line {
+  width: 400px;
+  height: 30px;
+  margin-top: 5px;
+  position: relative;
+}
+.line:before {
+  content: "";
+  width: 47%;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  height: 1px;
+  background-color: rgb(29, 27, 27, 0.2);
+}
+.line:after {
+  content: "";
+  width: 47%;
+  position: absolute;
+  top: 50%;
+  right: 0;
+  height: 1px;
+  background-color: rgb(29, 27, 27, 0.2);
+}
+.btn3 {
+  width: 400px;
+  height: 50px;
+  background-color: rgb(29, 155, 240) !important;
+}
+</style>
